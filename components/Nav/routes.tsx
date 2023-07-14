4185cbe9b0f491368cae03f7fe097979e3a8ed06
Kevin <kevin.lacarrubba@yahoo.com>
2023-07-14 04:00:29 -0400

    set up folder structure for nav bar

---
+++ components/Nav/routes.tsx
@@ -0,0 +1,29 @@
+export const NavItems = [
+    {
+        label: 'Home',
+        url: '/'
+    },
+    {
+        label: 'About',
+        url: '/about'
+    },
+    {
+        label: 'Products',
+        url: '/products'
+    }
+]
+
+export const MobileNavItems = [
+    {
+        label: 'Home',
+        url: '/'
+    },
+    {
+        label: 'About',
+        url: '/about'
+    },
+    {
+        label: 'Products',
+        url: '/products'
+    }
+]
