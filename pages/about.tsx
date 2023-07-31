ed48b0ad53d78c6dc9081acc3fd6c1a6fbfc0dd4
Kevin <kevin.lacarrubba@yahoo.com>
2023-07-31 16:00:09 -0400

    testing revalidate

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,14 +8,18 @@ export async function getStaticProps() {
         return {
             props: {
                 data: data
-            }
+            },
+            // Re-generate the page at most every 24 hours (you can adjust this based on your data update frequency)
+            revalidate: 86400 // 24 hours in seconds
         }
     } catch (error) {
         console.error(error)
         return {
             props: {
                 data: null
-            }
+            },
+            // Re-generate the page at most every 24 hours even if there's an error
+            revalidate: 86400 // 24 hours in seconds
         }
     }
 }
